b82cad20115ff2d6eb63c9bc5e40f10363a77782
lenixras <lenixras@gmail.com>
2025-07-08 14:43:40 +0300

    Update hash_password.py

--- volume/_data/hash_password.py
+++ volume/_data/hash_password.py
@@ -1,1 +1,1 @@
-from werkzeug.security import generate_password_hash; print(generate_password_hash("4BPUm5Mmn2qy4R2sCf46", method="pbkdf2:sha256"))
+from werkzeug.security import generate_password_hash; print(generate_password_hash("your_pwd", method="pbkdf2:sha256"))
